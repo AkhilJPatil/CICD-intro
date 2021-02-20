39b87f0b8a4292392df6bf7d6adf04b652baef7e
AkhilJPatil <akhilpatil04@gmail.com>
2021-02-20 17:36:43 +0100

    basic python script added

---
+++ hackerrank_DS_4.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 19/01/2021 16:11
+
+@author: Akhil
+
+ The function is expected to return an INTEGER_ARRAY.
+ The function accepts following parameters:
+  1. INTEGER d
+  2. INTEGER_ARRAY arr
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def rotateLeft(d, arr):
+    temp = []
+    for i in range(len(arr)):
+        if i+d >= len(arr):
+            temp.append(arr[i + d - len(arr)])
+        else:
+            temp.append(arr[i + d])
+
+    print(temp)
+    return temp
+
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+	print("result: "+str(result))
